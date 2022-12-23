e2f17657c4662c802a476a6695036c1bfd9e8a9b
nxmori1 <116832662+nxmori1@users.noreply.github.com>
2022-12-23 23:38:49 +0300

    инт в флоат
    a- Int, V-float
    V=переменная.ВкакойТипДанных().pow(число)

--- Решение/Задачи 22.10.22/1 задания/4.kt
+++ Решение/Задачи 22.10.22/1 задания/4.kt
@@ -3,8 +3,8 @@ import kotlin.math.pow
 fun main() {
     val scanner = Scanner(System.`in`)
     println("Введите стороны a")
-    val a= scanner.nextDouble() //Почему можно использовать только float и double для pow? Как использовать int?
-    val V=a.pow(3)
+    val a= scanner.nextInt()
+    val V=a.toFloat().pow(3)
     println("V = $V")
 
 }
